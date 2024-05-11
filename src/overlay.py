70b8edc67b19b402f742466a3f3f2fbe88432725
FiYir <>
2024-05-10 22:05:55 -0400

    FIX: correcting a few mistakes with the PIL and Pydantic changes

--- src/overlay.py
+++ src/overlay.py
@@ -3208,7 +3208,7 @@ class CreateCardToolTip(ScaledWindow):
                             raw_data = urllib.request.urlopen(
                                 image_request).read()
                             im = Image.open(io.BytesIO(raw_data))
-                            im.thumbnail(size, Image.LANCZOS)
+                            im.thumbnail(size, Image.Resampling.LANCZOS)
                             image = ImageTk.PhotoImage(im)
                             image_label = Label(tt_frame, image=image)
                             image_label.grid(
