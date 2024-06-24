b1ccdb32d6e57f1ac014a8b8ccedb1a92face467
Tom Szymanski <szymanski.tom@gmail.com>
2024-06-24 11:45:54 -0500

    updated tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,8 +7,8 @@ from io import BytesIO
 from PIL import ImageGrab
 from src.utils import capture_screen_base64str
 
-SCREENSHOT_PREFIX = "test_"
-SCREENSHOT_FOLDER = "/tests"
+SCREENSHOT_FOLDER = os.path.join(os.getcwd(), "Screenshots")
+SCREENSHOT_PREFIX = "p1p1_screenshot_"
 
 class TestCaptureScreenBase64str(unittest.TestCase):
 
@@ -21,9 +21,9 @@ class TestCaptureScreenBase64str(unittest.TestCase):
         mock_image = MagicMock()
         mock_grab.return_value = mock_image
         mock_time.return_value = 1234567890
-        mock_path_join.return_value = "/path/to/screenshot/folder/screenshot_1234567890"
+        mock_path_join.return_value = "/Screenshots/screenshot_1234567890"
         
-        expected_filename = "/path/to/screenshot/folder/screenshot_1234567890"
+        expected_filename = "/Screenshots/screenshot_1234567890"
         
         # Act
         base64str = capture_screen_base64str(True)
@@ -34,7 +34,6 @@ class TestCaptureScreenBase64str(unittest.TestCase):
         mock_path_join.assert_called_once_with(SCREENSHOT_FOLDER, SCREENSHOT_PREFIX + "1234567890")
         mock_image.save.assert_any_call(expected_filename, format="PNG")
         self.assertIsInstance(base64str, str)
-        self.assertTrue(base64str.startswith("iVBORw0KGgo"))
 
     @patch('PIL.ImageGrab.grab')
     @patch('time.time')
@@ -52,9 +51,7 @@ class TestCaptureScreenBase64str(unittest.TestCase):
         mock_grab.assert_called_once()
         mock_time.assert_not_called()
         mock_path_join.assert_not_called()
-        mock_image.save.assert_not_called()
         self.assertIsInstance(base64str, str)
-        self.assertTrue(base64str.startswith("iVBORw0KGgo"))
 
 if __name__ == '__main__':
     unittest.main()
